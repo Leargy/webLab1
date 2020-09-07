e255fc28efc75ce8df2f2ef06aa43d2472f21834
Anton Sushkevich <58341402+Leargy@users.noreply.github.com>
2020-09-07 20:00:27 +0300

    Add files via upload

--- js/send.js
+++ js/send.js
@@ -4,13 +4,25 @@ function formRequest() {
 // 	let rParams = document.getElementById('rField').value
 // 	console.log("X = " + xParams + " Y = " + yParams + " R = " + rParams)
 // 	console.log(typeof $('[serialize="true"]').serialize())
-	$.get("php/process.php", $('[serialize="true"]').serialize(), result=>{
-	    if(result) {
-	        let data = Cookies.get("data")
-	        console.log(data)
-            // fillTable(data)
-	    }else {
-	        alert("Currupted data")
-	    }
+	$.get({
+		url: "php/process.php",
+		data: $('[serialize="true"]').serialize(),
+		success: result=>{
+			    if(result) {
+			        let data = Cookies.get("data")
+			        console.log(data)
+		            // fillTable(data)
+			    }else {
+			        alert("Currupted data")
+			    }
 	});
+	// $.get("php/process.php", $('[serialize="true"]').serialize(), result=>{
+	//     if(result) {
+	//         let data = Cookies.get("data")
+	//         console.log(data)
+ //            // fillTable(data)
+	//     }else {
+	//         alert("Currupted data")
+	//     }
+	// });
 }
